79c7032067dbfb4f3049601abb302d410fc051fe
Bruce Potter <bp@us.ibm.com>
2017-03-07 11:33:29 -0500

    added msgs and bcs to scale test

--- src/test/bash/put/bctypes/bt1.sh
+++ src/test/bash/put/bctypes/bt1.sh
@@ -2,5 +2,5 @@
 if [[ $1 == "-raw" ]]; then parse=cat; else parse="jq -r ."; fi
 curl -# -w "%{http_code}" -X PUT -H 'Content-Type: application/json' -H 'Accept: application/json' -H "Authorization:Basic $EXCHANGE_USER:$EXCHANGE_PW" -d '{
   "description": "abc",
-  "details": "json escaped string"
+  "details": "{\"foo\":\"bar\"}"
 }' $EXCHANGE_URL_ROOT/v1/bctypes/bt1 | $parse

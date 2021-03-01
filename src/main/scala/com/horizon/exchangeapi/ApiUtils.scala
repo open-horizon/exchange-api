6e33675c4d9c385650aa9148e4bfd350dac97994
Bruce Potter <bp@us.ibm.com>
2021-03-01 14:10:34 -0500

    another tweak to lang debug statements
    Signed-off-by: Bruce Potter <bp@us.ibm.com>

--- src/main/scala/com/horizon/exchangeapi/ApiUtils.scala
+++ src/main/scala/com/horizon/exchangeapi/ApiUtils.scala
@@ -174,9 +174,9 @@ object ExchMsg {
   def translate(key: String, args: Any*): String = {
     try {
       //todo: remove these 2 debug statements
-      val lang = sys.env.getOrElse("HZN_EXCHANGE_LANG", sys.env.getOrElse("LANG", "en"))
-      if (lang.startsWith("zh") || lang.startsWith("pt")) println("using lang for msgs: "+lang)
-      
+      val exchLang = sys.env.getOrElse("HZN_EXCHANGE_LANG", sys.env.getOrElse("LANG", "en"))
+      if (exchLang.startsWith("zh") || exchLang.startsWith("pt")) println("using lang for msgs: "+exchLang)
+
       implicit val userLang: Lang = Lang(sys.env.getOrElse("HZN_EXCHANGE_LANG", sys.env.getOrElse("LANG", "en")))
       if (args.nonEmpty) {
         return Messages(key, args: _*)

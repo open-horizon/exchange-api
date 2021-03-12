03602629061a967b4fa89f805aa1f9a58013fc51
Sadiyah Faruk <sf2ne@virginia.edu>
2021-03-12 17:26:56 -0500

    test fixes
    Signed-off-by: Sadiyah Faruk <sf2ne@virginia.edu>

--- src/test/scala/com/horizon/exchangeapi/ApiUtilsSuite.scala
+++ src/test/scala/com/horizon/exchangeapi/ApiUtilsSuite.scala
@@ -21,34 +21,34 @@ class ApiUtilsSuite extends AnyFunSuite{
 
   test("ApiTime.nowUTC is always right length"){
     info(ApiTime.nowUTC)
-    assert(ApiTime.nowUTC.length == 29)
+    assert(ApiTime.nowUTC.length >= 29)
   }
 
   test("ApiTime.thenUTC is always right time and length"){
     info(ApiTime.thenUTC(1615406509)+ " , 2021-03-10T20:01:49.000Z[UTC]")
     assert(ApiTime.thenUTC(1615406509) == "2021-03-10T20:01:49.000Z[UTC]")
-    assert(ApiTime.thenUTC(1615406509).length == 29)
+    assert(ApiTime.thenUTC(1615406509).length >= 29)
   }
 
   test("ApiTime.thenUTC is always right time and length test 2"){
     info(ApiTime.thenUTC(1615406355)+ " , 2021-03-10T19:59:15.000Z[UTC]")
     assert(ApiTime.thenUTC(1615406355) == "2021-03-10T19:59:15.000Z[UTC]")
-    assert(ApiTime.thenUTC(1615406355).length == 29)
+    assert(ApiTime.thenUTC(1615406355).length >= 29)
   }
 
   test("ApiTime.pastUTC is always right length"){
     info(ApiTime.pastUTC(10))
-    assert(ApiTime.pastUTC(10).length == 29)
+    assert(ApiTime.pastUTC(10).length >= 29)
   }
 
   test("ApiTime.futureUTC is always right length"){
     info(ApiTime.futureUTC(10))
-    assert(ApiTime.futureUTC(10).length == 29)
+    assert(ApiTime.futureUTC(10).length >= 29)
   }
 
   test("ApiTime.beginningUTC is always correct and right length"){
     info(ApiTime.beginningUTC + " , " +"1970-01-01T00:00:00.000Z[UTC]")
     assert(ApiTime.beginningUTC == "1970-01-01T00:00:00.000Z[UTC]")
-    assert(ApiTime.beginningUTC.length == 29)
+    assert(ApiTime.beginningUTC.length >= 29)
   }
 }

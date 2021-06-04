2b309b6aab74656a88115851499b30310b7b527d
megha070 <varshney.megha070@gmail.com>
2021-06-05 02:25:56 +0530

    Added test for Business policy
    Signed-off-by: megha070 <varshney.megha070@gmail.com>
    Signed-off-by: Nathan Phelps <naphelps@us.ibm.com>

--- src/test/scala/com/horizon/exchangeapi/BusinessSuite.scala
+++ src/test/scala/com/horizon/exchangeapi/BusinessSuite.scala
@@ -665,6 +665,116 @@ class BusinessSuite extends AnyFunSuite {
     info("code: "+response.code+", response.body: "+response.body)
     assert(response.code === HttpCode.POST_OK.intValue)
   }
+  
+  test("POST /orgs/"+orgid+"/business/policies/SB1 - add SB1 Business Policy -- test the response body") {
+    val input = """{
+                      "label":"SB1",
+                      "description":"Test buspol with valid response body",
+                      "service":{"name":"ibm.gps","arch":"amd64","org":"IBM","serviceVersions":[{"version":"1.2.3"}]},
+                      "userInput":[{"serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","inputs":[{"name":"UI_STRING","value":"mystr"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}],
+                      "secretBinding": [{ "serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","serviceVersionRange": "x.y.z", "secrets": [{"secret1": "vaultsecret1"},{"secret2": "vaultsecret2"}]}],
+                      "properties":[{"name":"purpose","value":"location"}],
+                      "constraints":["a == b"]
+                    }""".stripMargin
+    val response = Http(URL+"/business/policies/SB1").postData(input).method("post").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.POST_OK.intValue)
+    //assert(response.body.contains("No usable value for service"))
+  }
+
+  test("POST /orgs/"+orgid+"/business/policies/SB2 - add SB2 Business Policy -- test if secretBinding is a mandatory field") {
+        val input = """{
+                          "label":"SB1",
+                          "description":"Test buspol with valid response body",
+                          "service":{"name":"ibm.gps","arch":"amd64","org":"IBM","serviceVersions":[{"version":"1.2.3"}]},
+                          "userInput":[{"serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","inputs":[{"name":"UI_STRING","value":"mystr"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}],
+                          "secretBinding": [],
+                          "properties":[{"name":"purpose","value":"location"}],
+                          "constraints":["a == b"]
+                        }""".stripMargin
+    val response = Http(URL+"/business/policies/SB2").postData(input).method("post").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.POST_OK.intValue)
+    //assert(response.body.contains("No usable value for service"))
+  }
+
+  test("POST /orgs/"+orgid+"/business/policies/SB3 - add SB3 Business Policy -- test the bad response body") {
+    val input = """{
+                      "label":"SB1",
+                      "description":"Test buspol with bad response body",
+                      "service":{"name":"ibm.gps","arch":"amd64","org":"IBM","serviceVersions":[{"version":"1.2.3"}]},
+                      "userInput":[{"serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","inputs":[{"name":"UI_STRING","value":"mystr"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}],
+                      "secretBinding": [{ "serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","serviceVersionRange": "x.y.z", "secrets":  { "FirstSecret": "secret1","Foo": "Bar" }}],
+                      "properties":[{"name":"purpose","value":"location"}],
+                      "constraints":["a == b"]
+                  }""".stripMargin
+    val response = Http(URL+"/business/policies/SB3").postData(input).method("post").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.BAD_INPUT.intValue)
+    //assert(response.body.contains("No usable value for service"))
+  }
+
+ test("PUT /orgs/"+orgid+"/business/policies/SB1 - add SB1 Business Policy -- test the response body") {
+    val input = """{
+                      "label":"SB1",
+                      "description":"Test buspol with valid response body",
+                      "service":{"name":"ibm.gps","arch":"amd64","org":"IBM","serviceVersions":[{"version":"1.2.3"}]},
+                      "userInput":[{"serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","inputs":[{"name":"UI_STRING","value":"mystr"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}],
+                      "secretBinding": [{ "serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","serviceVersionRange": "x.y.z", "secrets": [{"secret1": "vaultsecret1"},{"secret2": "vaultsecret2"}]}],
+                      "properties":[{"name":"purpose","value":"location"}],
+                      "constraints":["a == b"]
+                    }""".stripMargin
+    val response = Http(URL+"/business/policies/SB1").postData(input).method("put").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.PUT_OK.intValue)
+    //assert(response.body.contains("No usable value for service"))
+  }
+
+  test("PUT /orgs/"+orgid+"/business/policies/SB2 - add SB2 Business Policy -- test if secretBinding is a mandatory field") {
+        val input = """{
+                          "label":"SB1",
+                          "description":"Test buspol with valid response body",
+                          "service":{"name":"ibm.gps","arch":"amd64","org":"IBM","serviceVersions":[{"version":"1.2.3"}]},
+                          "userInput":[{"serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","inputs":[{"name":"UI_STRING","value":"mystr"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}],
+                          "secretBinding": [],
+                          "properties":[{"name":"purpose","value":"location"}],
+                          "constraints":["a == b"]
+                        }""".stripMargin
+    val response = Http(URL+"/business/policies/SB2").postData(input).method("put").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.PUT_OK.intValue)
+    //assert(response.body.contains("No usable value for service"))
+  }
+
+  test("PUT /orgs/"+orgid+"/business/policies/SB3 - add SB3 Business Policy -- test the bad response body") {
+    val input = """{
+                      "label":"SB1",
+                      "description":"Test buspol with bad response body",
+                      "service":{"name":"ibm.gps","arch":"amd64","org":"IBM","serviceVersions":[{"version":"1.2.3"}]},
+                      "userInput":[{"serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","inputs":[{"name":"UI_STRING","value":"mystr"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}],
+                      "secretBinding": [{ "serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","serviceVersionRange": "x.y.z", "secrets":  { "FirstSecret": "secret1","Foo": "Bar" }}],
+                      "properties":[{"name":"purpose","value":"location"}],
+                      "constraints":["a == b"]
+                  }""".stripMargin
+    val response = Http(URL+"/business/policies/SB3").postData(input).method("put").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.BAD_INPUT.intValue)
+    //assert(response.body.contains("No usable value for service"))
+  }
+
+    test("PATCH /orgs/"+orgid+"/business/policies/"+businessPolicy+" - the secretBinding") {
+    var jsonInput = """{"secretBinding": [{ "serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","serviceVersionRange": "x.y.z", "secrets": [{"secret1": "vaultsecret1"},{"secret2": "vaultsecret2"}]}]}"""
+    var response = Http(URL+"/business/policies/"+businessPolicy).postData(jsonInput).method("patch").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.PUT_OK.intValue)
+  }
+
+  test("PATCH /orgs/"+orgid+"/business/policies/"+businessPolicy+" - the secretBinding in invalid format") {
+    var jsonInput = """{"secretBinding": [{ "serviceOrgid":"BusinessSuiteTests","serviceUrl":"ibm.netspeed","serviceVersionRange": "x.y.z", "secrets":  { "FirstSecret": "secret1","Foo": "Bar" }}]}"""
+    var response = Http(URL+"/business/policies/"+businessPolicy).postData(jsonInput).method("patch").headers(CONTENT).headers(ACCEPT).headers(USERAUTH).asString
+    info("code: "+response.code+", response.body: "+response.body)
+    assert(response.code === HttpCode.BAD_INPUT.intValue)
+  }
 
   /*
   someday: when all test suites are run at the same time, there are sometimes timing problems them all setting config values...

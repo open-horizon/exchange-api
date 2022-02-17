c4b8cf92f7b9a0290747658af2f375cac5b0b4fe
lorenzoking <68292810+lorenzoking@users.noreply.github.com>
2022-02-17 16:12:46 -0500

    issue 557: nodemgmtstatus routes
    Signed-off-by: Nathan Phelps <naphelps@us.ibm.com>
    Signed-off-by: lorenzoking <lorenzomkingiii@ibm.com>

--- README.md
+++ README.md
@@ -300,6 +300,9 @@ Now you can disable root by setting `api.root.enabled` to `false` in `/etc/horiz
     - detect if a pattern is updated with service that has userInput w/o default values, and give warning
     - Consider changing all creates to POST, and update (via put/patch) return codes to 200
 
+## Changes in 2.95.0
+- Issue 557: Added node management status APIs
+
 ## Changes in 2.94.0
 - Issue 556: Updated the DB/Http schemas for Node Management Policies and routes.
 - Simplified Table Query syntax in the application's source.
--- src/test/scala/com/horizon/exchangeapi/route/node/TestNodeDeleteMgmtPolStatus.scala
+++ src/test/scala/com/horizon/exchangeapi/route/node/TestNodeDeleteMgmtPolStatus.scala
@@ -1,9 +1,8 @@
 package com.horizon.exchangeapi.route.node
 
-import com.horizon.exchangeapi.tables.{ManagementPoliciesTQ, ManagementPolicyRow, NMPStatus, NodeMgmtPolStatusRow, NodeMgmtPolStatuses, NodeRow, NodesTQ, OrgRow, OrgsTQ, ResourceChangesTQ, UserRow, UsersTQ}
-import com.horizon.exchangeapi.{ApiTime, ApiUtils, GetNMPStatusResponse, HttpCode, Role, TestDBConnection}
+import com.horizon.exchangeapi.tables.{ManagementPoliciesTQ, ManagementPolicyRow, NodeMgmtPolStatusRow, NodeMgmtPolStatuses, NodeRow, NodesTQ, OrgRow, OrgsTQ, ResourceChangesTQ, UserRow, UsersTQ}
+import com.horizon.exchangeapi.{ApiTime, ApiUtils, HttpCode, Role, TestDBConnection}
 import org.json4s.DefaultFormats
-import org.json4s.jackson.JsonMethods.parse
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import scalaj.http.Http
@@ -13,11 +12,11 @@ import scala.concurrent.Await
 import scala.concurrent.duration.{Duration, DurationInt}
 
 
-class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
+class TestNodeDeleteMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
   private val ACCEPT: (String, String) = ("Content-Type", "application/json")
   private val CONTENT: (String, String) = ACCEPT
   private val ROOTAUTH: (String, String) = ("Authorization", "Basic " + ApiUtils.encode(Role.superUser + ":" + sys.env.getOrElse("EXCHANGE_ROOTPW", "")))
-  private val URL: String = sys.env.getOrElse("EXCHANGE_URL_ROOT", "http://localhost:8080") + "/v1/orgs/" + "TestNodeGetAllMgmtPolStatus"
+  private val URL: String = sys.env.getOrElse("EXCHANGE_URL_ROOT", "http://localhost:8080") + "/v1/orgs/" + "TestNodeDeleteMgmtPolStatus"
   private val DBCONNECTION: TestDBConnection = new TestDBConnection
   private val AWAITDURATION: Duration = 15.seconds
   implicit val formats = DefaultFormats // Brings in default date formats etc.
@@ -27,7 +26,7 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
       description = "",
       label = "",
       lastUpdated = ApiTime.nowUTC,
-      orgId = "TestNodeGetAllMgmtPolStatus",
+      orgId = "TestNodeDeleteMgmtPolStatus",
       orgType = "",
       tags = None,
       limits = "")
@@ -40,49 +39,49 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
       label = "",
       lastUpdated = ApiTime.nowUTC,
       manifest = "",
-      managementPolicy = "TestNodeGetAllMgmtPolStatus/pol1",
-      orgid = "TestNodeGetAllMgmtPolStatus",
-      owner = "TestNodeGetAllMgmtPolStatus/u1",
+      managementPolicy = "TestNodeDeleteMgmtPolStatus/pol1",
+      orgid = "TestNodeDeleteMgmtPolStatus",
+      owner = "TestNodeDeleteMgmtPolStatus/u1",
       patterns = "",
       properties = "",
       start = ""),
-      ManagementPolicyRow(allowDowngrade = false,
-        constraints = "",
-        created = ApiTime.nowUTC,
-        description = "",
-        enabled = true,
-        label = "",
-        lastUpdated = ApiTime.nowUTC,
-        manifest = "",
-        managementPolicy = "TestNodeGetAllMgmtPolStatus/pol2",
-        orgid = "TestNodeGetAllMgmtPolStatus",
-        owner = "TestNodeGetAllMgmtPolStatus/u1",
-        patterns = "",
-        properties = "",
-        start = ""))
+    ManagementPolicyRow(allowDowngrade = false,
+      constraints = "",
+      created = ApiTime.nowUTC,
+      description = "",
+      enabled = true,
+      label = "",
+      lastUpdated = ApiTime.nowUTC,
+      manifest = "",
+      managementPolicy = "TestNodeDeleteMgmtPolStatus/pol2",
+      orgid = "TestNodeDeleteMgmtPolStatus",
+      owner = "TestNodeDeleteMgmtPolStatus/u1",
+      patterns = "",
+      properties = "",
+      start = ""))
 
   private val TESTNODE: NodeRow =
     NodeRow(arch = "amd64",
-      id = "TestNodeGetAllMgmtPolStatus/n1",
+      id = "TestNodeDeleteMgmtPolStatus/n1",
       heartbeatIntervals = """{"minInterval":6,"maxInterval":15,"intervalAdjustment":2}""",
       lastHeartbeat = Some(ApiTime.nowUTC),
       lastUpdated = ApiTime.nowUTC,
       msgEndPoint = "messageEndpoint",
       name = "rpin1-normal",
       nodeType = "device",
-      orgid = "TestNodeGetAllMgmtPolStatus",
-      owner = "TestNodeGetAllMgmtPolStatus/u1",
-      pattern = "TestNodeGetAllMgmtPolStatus/p1",
+      orgid = "TestNodeDeleteMgmtPolStatus",
+      owner = "TestNodeDeleteMgmtPolStatus/u1",
+      pattern = "TestNodeDeleteMgmtPolStatus/p1",
       publicKey = "key",
       regServices = """[{"url":"NodesSuiteTests/horizon.sdr","numAgreements":1,"configState":"active","policy":"{json policy for n1 sdr}","properties":[{"name":"arch","value":"arm","propType":"string","op":"in"},{"name":"memory","value":"300","propType":"int","op":">="},{"name":"version","value":"1.0.0","propType":"version","op":"in"},{"name":"agreementProtocols","value":"ExchangeAutomatedTest","propType":"list","op":"in"},{"name":"dataVerification","value":"true","propType":"boolean","op":"="}]},{"url":"NodesSuiteTests/horizon.netspeed","numAgreements":1,"configState":"active","policy":"{json policy for n1 netspeed}","properties":[{"name":"arch","value":"arm","propType":"string","op":"in"},{"name":"agreementProtocols","value":"ExchangeAutomatedTest","propType":"list","op":"in"},{"name":"version","value":"1.0.0","propType":"version","op":"in"}]}]""",
       softwareVersions = """{"horizon":"3.2.1"}""",
-      token = "", // TestNodeGetAllMgmtPolStatus/n1:n1pw
+      token = "", // TestNodeDeleteMgmtPolStatus/n1:n1pw
       userInput = """[{"serviceOrgid":"NodesSuiteTests","serviceUrl":"horizon.sdr","serviceArch":"amd64","serviceVersionRange":"[0.0.0,INFINITY)","inputs":[{"name":"UI_STRING","value":"mystr - updated"},{"name":"UI_INT","value":5},{"name":"UI_BOOLEAN","value":true}]}]""")
 
   private val TESTNODEMGMTPOLSTATUSES: Seq[NodeMgmtPolStatusRow] =
     Seq(NodeMgmtPolStatusRow(errorMessage = "pol1 description test",
-      node = "TestNodeGetAllMgmtPolStatus/n1",
-      policy = "TestNodeGetAllMgmtPolStatus/pol1",
+      node = "TestNodeDeleteMgmtPolStatus/n1",
+      policy = "TestNodeDeleteMgmtPolStatus/pol1",
       status = "Success",
       endTime = "",
       actualStartTime = ApiTime.nowUTC,
@@ -92,8 +91,8 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
       configurationVersion = "",
       softwareVersion = ""),
       NodeMgmtPolStatusRow(errorMessage = "pol2 description test",
-        node = "TestNodeGetAllMgmtPolStatus/n1",
-        policy = "TestNodeGetAllMgmtPolStatus/pol2",
+        node = "TestNodeDeleteMgmtPolStatus/n1",
+        policy = "TestNodeDeleteMgmtPolStatus/pol2",
         status = "Fail",
         endTime = "",
         actualStartTime = ApiTime.nowUTC,
@@ -107,11 +106,11 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
     UserRow(admin = false,
       hubAdmin = false,
       email = "",
-      hashedPw = "$2a$10$fEe00jBiITDA7RnRUGFH.upsISQ3cm93pdvkbJaFr5ZC/5kxhyZ4i", // TestNodeGetAllMgmtPolStatus/u1:u1pw
+      hashedPw = "$2a$10$fEe00jBiITDA7RnRUGFH.upsISQ3cm93pdvkbJaFr5ZC/5kxhyZ4i", // TestNodeDeleteMgmtPolStatus/u1:u1pw
       lastUpdated = ApiTime.nowUTC,
-      orgid = "TestNodeGetAllMgmtPolStatus",
+      orgid = "TestNodeDeleteMgmtPolStatus",
       updatedBy = "",
-      username = "TestNodeGetAllMgmtPolStatus/u1")
+      username = "TestNodeDeleteMgmtPolStatus/u1")
 
 
 
@@ -126,8 +125,8 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
 
   // Teardown testing harness and cleanup.
   override def afterAll(): Unit = {
-    Await.ready(DBCONNECTION.getDb.run(ResourceChangesTQ.filter(_.orgId startsWith "TestNodeGetAllMgmtPolStatus").delete andThen
-      OrgsTQ.filter(_.orgid startsWith "TestNodeGetAllMgmtPolStatus").delete), AWAITDURATION)
+    Await.ready(DBCONNECTION.getDb.run(ResourceChangesTQ.filter(_.orgId startsWith "TestNodeDeleteMgmtPolStatus").delete andThen
+      OrgsTQ.filter(_.orgid startsWith "TestNodeDeleteMgmtPolStatus").delete), AWAITDURATION)
 
     DBCONNECTION.getDb.close()
   }
@@ -142,7 +141,6 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
       Await.result(DBCONNECTION.getDb.run(ManagementPoliciesTQ.filter(_.managementPolicy inSet testData.map(_.managementPolicy)).delete), AWAITDURATION)
   }
 
-  // Management Policy Statuses that are dynamically needed, specific to the test case.
   def fixtureNodeMgmtPolStatus(testCode: Seq[NodeMgmtPolStatusRow] => Any, testData: Seq[NodeMgmtPolStatusRow]): Any = {
     try {
       Await.result(DBCONNECTION.getDb.run(NodeMgmtPolStatuses ++= testData), AWAITDURATION)
@@ -152,21 +150,41 @@ class TestNodeGetAllMgmtPolStatus extends AnyFunSuite with BeforeAndAfterAll {
       Await.result(DBCONNECTION.getDb.run(NodeMgmtPolStatuses.filter(_.policy inSet testData.map(_.policy)).delete), AWAITDURATION)
   }
 
-
-  test("GET /orgs/TestNodeGetAllMgmtPolStatus/nodes/n1/managementStatus - Default") {
+  test("DELETE /orgs/TestNodeDeleteMgmtPolStatus/nodes/n1/managementStatus/pol1 - Default") {
     fixtureNodeMgmtPol(
       _ => {
         fixtureNodeMgmtPolStatus(
           _ => {
-            val response = Http(URL + "/nodes/n1/managementStatus").method("get").headers(CONTENT).headers(ACCEPT).headers(ROOTAUTH).asString
-            info("code: " + response.code + ", response.body: " + response.body)
-            assert(response.code === HttpCode.OK.intValue)
-            val resp: GetNMPStatusResponse = parse(response.body).extract[GetNMPStatusResponse]
-            assert(resp.policy.size == 2)
+            val response = Http(URL + "/nodes/n1/managementStatus/pol1").method("delete").headers(CONTENT).headers(ACCEPT).headers(ROOTAUTH).asString
+            info("Code: " + response.code)
+            info("Body: " + response.body)
+            assert(response.code === HttpCode.DELETED.intValue)
+
+            val status: Seq[NodeMgmtPolStatusRow] = Await.result(DBCONNECTION.getDb.run(NodeMgmtPolStatuses.getNodeMgmtPolStatus(TESTNODE.id, "TestNodeDeleteMgmtPolStatus/pol1").result), AWAITDURATION)
+            assert(status.length === 0)
 
           }, TESTNODEMGMTPOLSTATUSES)
       }, TESTMANAGEMENTPOLICY)
   }
+
+
+  test("DELETE /orgs/TestNodeDeleteMgmtPolStatus/nodes/n1/managementStatus/pol2 - Default") {
+    fixtureNodeMgmtPol(
+      _ => {
+        fixtureNodeMgmtPolStatus(
+          _ => {
+            val response = Http(URL + "/nodes/n1/managementStatus/pol2").method("delete").headers(CONTENT).headers(ACCEPT).headers(ROOTAUTH).asString
+            info("Code: " + response.code)
+            info("Body: " + response.body)
+            assert(response.code === HttpCode.DELETED.intValue)
+
+            val status: Seq[NodeMgmtPolStatusRow] = Await.result(DBCONNECTION.getDb.run(NodeMgmtPolStatuses.getNodeMgmtPolStatus(TESTNODE.id, "TestNodeDeleteMgmtPolStatus/pol2").result), AWAITDURATION)
+            assert(status.length === 0)
+
+          }, TESTNODEMGMTPOLSTATUSES)
+      }, TESTMANAGEMENTPOLICY)
+  }
+
 
 
 

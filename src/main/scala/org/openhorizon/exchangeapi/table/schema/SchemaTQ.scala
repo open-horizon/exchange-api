1f0d6f7de47d7383aff315befd766f0b1ab40569
Nathan Phelps <naphelps@us.ibm.com>
2025-09-03 10:27:46 -0400

    Fixed syntax issues in DDL for database migration 56.
    Signed-off-by: Nathan Phelps <naphelps@us.ibm.com>

--- src/main/scala/org/openhorizon/exchangeapi/table/schema/SchemaTQ.scala
+++ src/main/scala/org/openhorizon/exchangeapi/table/schema/SchemaTQ.scala
@@ -232,19 +232,19 @@ object SchemaTQ extends TableQuery(new SchemaTable(_)){
       case 56 => // v2.127.0, v2.148.0
         DBIO.seq(
           // Add new User primary key columns to downstream tables for reference.
-          // v2.148.0 - Moved NOT NULL constraint to post migration.
-          sqlu"ALTER TABLE public.agbots ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
-          sqlu"ALTER TABLE public.businesspolicies ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
-          sqlu"ALTER TABLE public.managementpolicies ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
-          sqlu"ALTER TABLE public.nodes ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
-          sqlu"ALTER TABLE public.patterns ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
-          sqlu"ALTER TABLE public.services ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
+          // v2.148.0 - Moved NOT NULL constraint to post migration
+          sqlu"ALTER TABLE IF EXISTS public.agbots ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.businesspolicies ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.managementpolicies ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.nodes ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.patterns ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.services ADD COLUMN IF NOT EXISTS owner_uuid UUID NULL;",
           
           // Add new columns to our existing Users table. These need to be nullable here.
-          sqlu"ALTER TABLE public.users ADD COLUMN IF NOT EXISTS modified_at TIMESTAMP NULL;",
-          sqlu"ALTER TABLE public.users ADD COLUMN IF NOT EXISTS modified_by UUID NULL;",
-          sqlu"""ALTER TABLE public.users ADD COLUMN IF NOT EXISTS "user" UUID NULL;""",
-          sqlu"ALTER TABLE public.users ADD COLUMN IF NOT EXISTS username_new VARCHAR NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.users ADD COLUMN IF NOT EXISTS modified_at TIMESTAMP NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.users ADD COLUMN IF NOT EXISTS modified_by UUID NULL;",
+          sqlu"""ALTER TABLE IF EXISTS public.users ADD COLUMN IF NOT EXISTS "user" UUID NULL;""",
+          sqlu"ALTER TABLE IF EXISTS public.users ADD COLUMN IF NOT EXISTS username_new VARCHAR NULL;",
           
           // Fill-in our new columns.
           // No great means of converting backwards from a string to a timestamp. At best these can only be parsed approximations.
@@ -322,39 +322,40 @@ object SchemaTQ extends TableQuery(new SchemaTable(_)){
               """,
           
           // Drop existing foreign keys.
-          sqlu"ALTER TABLE public.agbots DROP CONSTRAINT IF EXISTS user_fk;",
-          sqlu"ALTER TABLE public.businesspolicies DROP CONSTRAINT IF EXISTS user_fk;",
-          sqlu"ALTER TABLE public.managementpolicies DROP CONSTRAINT IF EXISTS user_fk;",
-          sqlu"ALTER TABLE public.nodes DROP CONSTRAINT IF EXISTS user_fk;",
-          sqlu"ALTER TABLE public.patterns DROP CONSTRAINT IF EXISTS user_fk;",
-          sqlu"ALTER TABLE public.services DROP CONSTRAINT IF EXISTS user_fk;",
+          sqlu"ALTER TABLE IF EXISTS public.agbots DROP CONSTRAINT IF EXISTS user_fk;",
+          sqlu"ALTER TABLE IF EXISTS public.businesspolicies DROP CONSTRAINT IF EXISTS user_fk;",
+          sqlu"ALTER TABLE IF EXISTS public.managementpolicies DROP CONSTRAINT IF EXISTS user_fk;",
+          sqlu"ALTER TABLE IF EXISTS public.nodes DROP CONSTRAINT IF EXISTS user_fk;",
+          sqlu"ALTER TABLE IF EXISTS public.patterns DROP CONSTRAINT IF EXISTS user_fk;",
+          sqlu"ALTER TABLE IF EXISTS public.services DROP CONSTRAINT IF EXISTS user_fk;",
           
           // Drop existing User reference columns
-          sqlu"ALTER TABLE public.agbots DROP COLUMN IF EXISTS owner;",
-          sqlu"ALTER TABLE public.businesspolicies DROP COLUMN IF EXISTS owner;",
-          sqlu"ALTER TABLE public.managementpolicies DROP COLUMN IF EXISTS owner;",
-          sqlu"ALTER TABLE public.nodes DROP COLUMN IF EXISTS owner;",
-          sqlu"ALTER TABLE public.patterns DROP COLUMN IF EXISTS owner;",
-          sqlu"ALTER TABLE public.services DROP COLUMN IF EXISTS owner;",
+          sqlu"ALTER TABLE IF EXISTS public.agbots DROP COLUMN IF EXISTS owner;",
+          sqlu"ALTER TABLE IF EXISTS public.businesspolicies DROP COLUMN IF EXISTS owner;",
+          sqlu"ALTER TABLE IF EXISTS public.managementpolicies DROP COLUMN IF EXISTS owner;",
+          sqlu"ALTER TABLE IF EXISTS public.nodes DROP COLUMN IF EXISTS owner;",
+          sqlu"ALTER TABLE IF EXISTS public.patterns DROP COLUMN IF EXISTS owner;",
+          sqlu"ALTER TABLE IF EXISTS public.services DROP COLUMN IF EXISTS owner;",
           
           // Rename new User reference columns
-          sqlu"ALTER TABLE public.agbots RENAME COLUMN owner_uuid TO owner;",
-          sqlu"ALTER TABLE public.businesspolicies RENAME COLUMN owner_uuid TO owner;",
-          sqlu"ALTER TABLE public.managementpolicies RENAME COLUMN owner_uuid TO owner;",
-          sqlu"ALTER TABLE public.nodes RENAME COLUMN owner_uuid TO owner;",
-          sqlu"ALTER TABLE public.patterns RENAME COLUMN owner_uuid TO owner;",
-          sqlu"ALTER TABLE public.services RENAME COLUMN owner_uuid TO owner;",
+          sqlu"ALTER TABLE IF EXISTS public.agbots RENAME COLUMN owner_uuid TO owner;",
+          sqlu"ALTER TABLE IF EXISTS public.businesspolicies RENAME COLUMN owner_uuid TO owner;",
+          sqlu"ALTER TABLE IF EXISTS public.managementpolicies RENAME COLUMN owner_uuid TO owner;",
+          sqlu"ALTER TABLE IF EXISTS public.nodes RENAME COLUMN owner_uuid TO owner;",
+          sqlu"ALTER TABLE IF EXISTS public.patterns RENAME COLUMN owner_uuid TO owner;",
+          sqlu"ALTER TABLE IF EXISTS public.services RENAME COLUMN owner_uuid TO owner;",
           
           // Add not null constraints to owner columns post migration
           // v2.148.0
-          sqlu"ALTER TABLE public.agbots ALTER COLUMN owner SET NOT NULL;",
-          sqlu"ALTER TABLE public.businesspolicies ALTER COLUMN owner SET NOT NULL;",
-          sqlu"ALTER TABLE public.managementpolicies ALTER COLUMN owner SET NOT NULL;",
-          sqlu"ALTER TABLE public.nodes ALTER COLUMN owner SET NOT NULL;",
-          sqlu"ALTER TABLE public.patterns ALTER COLUMN owner SET NOT NULL;",
-          sqlu"ALTER TABLE public.services ALTER COLUMN owner SET NOT NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.agbots ALTER COLUMN owner SET NOT NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.businesspolicies ALTER COLUMN owner SET NOT NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.managementpolicies ALTER COLUMN owner SET NOT NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.nodes ALTER COLUMN owner SET NOT NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.patterns ALTER COLUMN owner SET NOT NULL;",
+          sqlu"ALTER TABLE IF EXISTS public.services ALTER COLUMN owner SET NOT NULL;",
           
           // Create our new Users table
+          // v2.148.0 - Fixed syntax errors
           sqlu"""
                 CREATE TABLE IF NOT EXISTS public.users_schema_56 (
                    created_at timestamp NOT NULL,
@@ -370,8 +371,8 @@ object SchemaTQ extends TableQuery(new SchemaTable(_)){
                    username VARCHAR NOT NULL,
                    CONSTRAINT users_pk PRIMARY KEY ("user"),
                    CONSTRAINT users_uk UNIQUE (organization, username),
-                   CONSTRAINT users_org_fk FOREIGN KEY (organization) REFERENCES public.orgs(orgid) ON DELETE CASCADE ON UPDATE CASCADE
-                   CONSTRAINT users_usr_fk FOREIGN KEY (modified_by) REFERENCES public.users("user") ON DELETE SET NULL ON UPDATE CASCADE
+                   CONSTRAINT users_org_fk FOREIGN KEY (organization) REFERENCES public.orgs(orgid) ON DELETE CASCADE ON UPDATE CASCADE,
+                   CONSTRAINT users_usr_fk FOREIGN KEY (modified_by) REFERENCES public.users("user") ON DELETE SET NULL ON UPDATE CASCADE INITIALLY DEFERRED,
                    CONSTRAINT users_root_check CHECK (((organization = 'root') AND (username = 'root')) OR (NOT (is_hub_admin AND is_org_admin))));
               """,
           
@@ -407,19 +408,20 @@ object SchemaTQ extends TableQuery(new SchemaTable(_)){
           sqlu"ALTER TABLE IF EXISTS public.users_schema_56 RENAME TO users;",
           
           // Recreate foreign key references
-          sqlu"""ALTER TABLE public.agbots ADD CONSTRAINT agbots_user_fk FOREIGN KEY (owner) REFERENCES public.users ("user");""",
-          sqlu"""ALTER TABLE public.businesspolicies ADD CONSTRAINT deploypol_user_fk FOREIGN KEY (owner) REFERENCES public.users ("user");""",
-          sqlu"""ALTER TABLE public.managementpolicies ADD CONSTRAINT mgmtpol_user_fk FOREIGN KEY (owner) REFERENCES public.users ("user");""",
-          sqlu"""ALTER TABLE public.nodes ADD CONSTRAINT nodes_user_fk FOREIGN KEY (owner) REFERENCES public.users ("user");""",
-          sqlu"""ALTER TABLE public.patterns ADD CONSTRAINT pattrns_user_fk FOREIGN KEY (owner) REFERENCES public.users ("user");""",
-          sqlu"""ALTER TABLE public.services ADD CONSTRAINT svcs_user_fk FOREIGN KEY (owner) REFERENCES public.users ("user");""",
+          sqlu"""ALTER TABLE IF EXISTS public.agbots ADD CONSTRAINT agbots_user_fk FOREIGN KEY (owner) REFERENCES public.users("user");""",
+          sqlu"""ALTER TABLE IF EXISTS public.businesspolicies ADD CONSTRAINT deploypol_user_fk FOREIGN KEY (owner) REFERENCES public.users("user");""",
+          sqlu"""ALTER TABLE IF EXISTS public.managementpolicies ADD CONSTRAINT mgmtpol_user_fk FOREIGN KEY (owner) REFERENCES public.users("user");""",
+          sqlu"""ALTER TABLE IF EXISTS public.nodes ADD CONSTRAINT nodes_user_fk FOREIGN KEY (owner) REFERENCES public.users("user");""",
+          sqlu"""ALTER TABLE IF EXISTS public.patterns ADD CONSTRAINT pattrns_user_fk FOREIGN KEY (owner) REFERENCES public.users("user");""",
+          sqlu"""ALTER TABLE IF EXISTS public.services ADD CONSTRAINT svcs_user_fk FOREIGN KEY (owner) REFERENCES public.users("user");""",
           
           // Create missing foreign key references
-          sqlu"""ALTER TABLE public.nodepolicies ADD CONSTRAINT node_deploy_pol_fk_nodes FOREIGN KEY (nodeid) REFERENCES public.nodes ("id");""",
-          sqlu"""ALTER TABLE public.nodeerror ADD CONSTRAINT node_error_fk_nodes FOREIGN KEY (nodeid) REFERENCES public.nodes ("id");""",
-          sqlu"""ALTER TABLE public.management_policy_status_node ADD CONSTRAINT node_mgmt_pol_status_fk_nodes FOREIGN KEY (node) REFERENCES public.nodes ("id");""",
+          sqlu"""ALTER TABLE public.nodepolicies ADD CONSTRAINT node_deploy_pol_fk_nodes FOREIGN KEY (nodeid) REFERENCES public.nodes("id");""",
+          sqlu"""ALTER TABLE public.nodeerror ADD CONSTRAINT node_error_fk_nodes FOREIGN KEY (nodeid) REFERENCES public.nodes("id");""",
+          sqlu"""ALTER TABLE public.management_policy_status_node ADD CONSTRAINT node_mgmt_pol_status_fk_nodes FOREIGN KEY (node) REFERENCES public.nodes("id");""",
           
           // Create missing indexes on foreign keys
+          // v2.148.0 - Fixed syntax errors
           sqlu"""CREATE INDEX IF NOT EXISTS idx_agbot_agree_fk_agbots ON public.agbotagreements(agbotid);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_agbot_deploy_pol_fk_agbots ON public.agbotbusinesspols(agbotid);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_agbot_msg_fk_agbots ON public.agbotmsgs(agbotid);""",
@@ -437,14 +439,14 @@ object SchemaTQ extends TableQuery(new SchemaTable(_)){
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_deploy_pol_fk_nodes ON public.nodepolicies(nodeid);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_error_fk_nodes ON public.nodeerror(nodeId);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_grp_assgn_fk_nodes ON public.node_group_assignment(node);""",
-          sqlu"""CREATE INDEX IF NOT EXISTS idx_node_grp_assgn_fk_node_grps ON public.node_group_assignment(group);""",
+          sqlu"""CREATE INDEX IF NOT EXISTS idx_node_grp_assgn_fk_node_grps ON public.node_group_assignment("group");""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_mgmt_pol_stat_fk_mgmt_pols ON public.management_policy_status_node(policy);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_mgmt_pol_stat_fk_nodes ON public.management_policy_status_node(node);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_msg_fk_agbots ON public.nodemsgs(agbotid);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_msg_fk_nodes ON public.nodemsgs(nodeid);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_node_status_fk_nodes ON public.nodestatus(nodeId);""",
-          sqlu"""CREATE INDEX IF NOT EXISTS idx_node_fk_orgs ON public.nodemsgs(orgid);""",
-          sqlu"""CREATE INDEX IF NOT EXISTS idx_node_fk_users ON public.nodemsgs(users);""",
+          //sqlu"""CREATE INDEX IF NOT EXISTS idx_node_fk_orgs ON public.nodemsgs(orgid);""",
+          //sqlu"""CREATE INDEX IF NOT EXISTS idx_node_fk_users ON public.nodemsgs(users);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_search_offset_pol_fk_deploy_pols ON public.search_offset_policy(policy);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_serv_dock_auth_fk_services ON public.servicedockauths(serviceid);""",
           sqlu"""CREATE INDEX IF NOT EXISTS idx_serv_key_fk_services ON public.servicekeys(serviceid);""",
@@ -490,7 +492,7 @@ object SchemaTQ extends TableQuery(new SchemaTable(_)){
     }
   }
 
-  val latestSchemaVersion: Int = 60    // NOTE: THIS MUST BE CHANGED WHEN YOU ADD TO getUpgradeSchemaStep() above
+  val latestSchemaVersion: Int = 56    // NOTE: THIS MUST BE CHANGED WHEN YOU ADD TO getUpgradeSchemaStep() above
   val latestSchemaDescription: String = ""
   // Note: if you need to manually set the schema number in the db lower: update schema set schemaversion = 12 where id = 0;
 
